4f427012141fb310d95cedaf625b340bcf67cdff
mahdi aliabadi <aiabadimahdi@gmail.com>
2022-07-12 13:36:09 +0430

    get rid of useless keywords in filenames

--- app/main.py
+++ app/main.py
@@ -8,9 +8,22 @@ def scan_items(base: str, files_list: list[str]):
     for path in files_list:
         absolute_path = base + "/" + path
         if os.path.isfile(absolute_path):
-            path = '.'.join(path.split(".")[:-1])
+            path = normalize_filename(path)
             print(path)
-            f.write("<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path.replace(".", "+"), path))
+            f.write(
+                "<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path, path)
+            )
+
+
+def normalize_filename(name: str):
+    split_start_index = 500
+    for keyword in config.ignore_keywords:
+        keyword_start_index = None
+        if (keyword_start_index := name.lower().find(keyword)) != -1:
+            split_start_index = min(keyword_start_index, split_start_index)
+
+    print(split_start_index)
+    return name[:split_start_index].replace(".", " ").replace("_", " ")
 
 
 scan_items(config.library_dir, os.listdir(config.library_dir))
