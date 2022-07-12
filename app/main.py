b339ee42fbbd401cef119cefb41af3590c6c7bc2
mahdi aliabadi <aiabadimahdi@gmail.com>
2022-07-12 12:50:19 +0430

    add target=_blank to <a>

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ import config
 
 
 def scan_items(base: str, files_list: list[str]):
-    f = open("../films.html", "w")
+    f = open("films.html", "w")
     for path in files_list:
         absolute_path = base + "/" + path
         if os.path.isfile(absolute_path):
-            f.write("<a href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path.replace(".", "+"), path))
+            path = '.'.join(path.split(".")[:-1])
+            print(path)
+            f.write("<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path.replace(".", "+"), path))
 
 
 scan_items(config.library_dir, os.listdir(config.library_dir))
