856d499058cd8b5a32440588e96d6670a98350c9
mahdi aliabadi <aiabadimahdi@gmail.com>
2022-07-12 14:40:18 +0430

    support recursive directory scan

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ import config
 
 
 def scan_items(base: str, files_list: list[str]):
-    f = open("films.html", "w")
+    f = open("films.html", "a")
     for path in files_list:
         absolute_path = base + "/" + path
-        if os.path.isfile(absolute_path):
+        if os.path.isfile(absolute_path) and not path.endswith(".lnk"):
+            # print(path)
             path = normalize_filename(path)
             print(path)
             f.write(
                 "<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path, path)
             )
-
+        elif os.path.isdir(absolute_path):
+            scan_items(absolute_path, os.listdir(absolute_path))
 
 def normalize_filename(name: str):
     split_start_index = 500
@@ -22,7 +24,6 @@ def normalize_filename(name: str):
         if (keyword_start_index := name.lower().find(keyword)) != -1:
             split_start_index = min(keyword_start_index, split_start_index)
 
-    print(split_start_index)
     return name[:split_start_index].replace(".", " ").replace("_", " ")
 
 
