6e70fa7acf2eca06f3b963122629b56c7fd12891
mahdi aliabadi <aiabadimahdi@gmail.com>
2022-07-15 00:51:11 +0430

    generate search links for persian and english subtitle

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import config
 
 
 def scan_items(base: str, files_list: list[str]):
-    f = open("films.html", "a")
+    f = open("films.html", "a", encoding="utf-8")
     for path in files_list:
         absolute_path = base + "/" + path
         if os.path.isfile(absolute_path) and not path.endswith(".lnk"):
@@ -12,7 +12,10 @@ def scan_items(base: str, files_list: list[str]):
             path = normalize_filename(path)
             print(path)
             f.write(
-                "<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">{}</a><br>\r\n".format(path, path)
+                "<p>{}</p>".format(path) +
+                "<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">google</a><br>".format(path) +
+                "<a target=\"_blank\" href=\"https://www.google.com/search?q={}\">زیرنویس فارسی</a><br>\r\n".format(path + "زیرنویس فارسی ") +
+                "<a target=\"_blank\" href=\"https://www.google.com/search?q={} subtitle\">زیرنویس انگلیسی</a><br>\r\n".format(path)
             )
         elif os.path.isdir(absolute_path):
             scan_items(absolute_path, os.listdir(absolute_path))
