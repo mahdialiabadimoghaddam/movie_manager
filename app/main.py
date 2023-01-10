532b10efa7cbacc93a95e1c6bd999ec9af6e70a6
mahdi <aiabadimahdi@gmail.com>
2023-01-10 17:15:13 +0330

    imporove file scanning and processing them 	- define a function that scans the library recursively and ignores files with unwanted type 	- update scan_items() 	- update generate_html()

--- app/main.py
+++ app/main.py
@@ -3,24 +3,19 @@ from fetch_scores import fetch_scores_html
 
 import config
 
+ignoredextensions = []
 
-def scan_items(base: str, folder_name: str, files_list: list[str]):
-    for path in files_list:
-        absolute_path = base + "/" + path
-        if os.path.isfile(absolute_path) and not path.endswith(".lnk"):
-            print(path)
-
-            writeHTML(
-                generate_html(normalize_filename(path), folder_name, path)
-            )
-        elif os.path.isdir(absolute_path):
-            scan_items(absolute_path, os.path.basename(
-                absolute_path), os.listdir(absolute_path))
+def scan_items(files_list: list[tuple]):
+    for filename, path in files_list:
+        normalized_filename = normalize_filename(filename)
+        generated_html = generate_html(normalized_filename, path)
+        writeHTML(generated_html)
+        print("\n\n")
 
 
-def generate_html(path: str, folder_name: str, file_name: str):
+def generate_html(filename: str, path: str):
     return config.HTML_BOX_TEMPLATE.format(
-        folder_name + "/ " + path, file_name, fetch_scores_html(path), path, path, path
+        filename, path, fetch_scores_html(filename), filename, filename, filename
     )
 
 
@@ -35,6 +30,18 @@ def normalize_filename(name: str):
 
     return " ".join(name).replace('(', '').replace(')', '')
 
+def listmovies(dir):
+    r = []
+    for root, _, files in os.walk(dir):
+        for name in files:
+            name, ext = os.path.splitext(name)
+            ext = ext.lower()
+            if ext in config.FILE_TYPES:
+                r.append((name, os.path.join(root, name)))
+            elif not ext in ignoredextensions:
+                # global ignoredextensions
+                ignoredextensions.append(ext)
+    return r
 
 def writeHTML(text, mode='a'):
     with open(f"{config.SAVE_TO}/films.html", mode, encoding="utf-8") as f:
@@ -42,5 +49,6 @@ def writeHTML(text, mode='a'):
 
 
 writeHTML(config.HTML_FILE_TEMPLATE, 'w')
-scan_items(config.LIBRARY_DIR, os.path.basename(config.LIBRARY_DIR), os.listdir(config.LIBRARY_DIR))
+scan_items(listmovies(config.LIBRARY_DIR))
 
+print(ignoredextensions)
